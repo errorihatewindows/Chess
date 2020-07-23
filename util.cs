0f60c8ce6edf48bfdc41a969ffe846bdd9984a32
Errorihatewindows <Daily-craft@hotmail.de>
2020-07-23 23:15:14 +0200

    branch created

---
+++ util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Board = System.Collections.Generic.Dictionary<System.Tuple<int, int>, char>;
+using Coord = System.Tuple<int, int>;
+
+namespace Chess
+{
+    class util
+    {
+    }
+}
